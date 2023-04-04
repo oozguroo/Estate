86e7e0de1e8e0c0d9faf7b578796bf678d6aaaaf
oozguroo <ozgurraykut@gmail.com>
2023-04-04 16:31:56 +0300

    part2

--- API/Program.cs
+++ API/Program.cs
@@ -11,11 +11,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder =>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
